62b52ae625f1c41f0f6bc5a5b0d1e3d2703dc42a
Glitch (hello-express) <none>
2021-03-15 06:07:49 +0000

    🖐🛤 Checkpoint ./server.js:28994970/676

--- server.js
+++ server.js
@@ -31,20 +31,8 @@ if (msg.content === 'a!help') {
     msg.channel.send(' ``a!slmt pagi`` |  ``a!slmt siang``  |  ``a!slmt malam`` |  ``a!p``   |  ``a!av`` ');
   }  
   
-    if (msg.content === 'how to embed') {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
-    const embed = new messageembed()
-      // Set the title of the field
-      .setTitle('A slick little embed')
-      // Set the color of the embed
-      .setColor(0xff0000)
-      // Set the main content of the embed
-      .setDescription('Hello, this is a slick embed!');
-    // Send the embed to the same channel as the message
-    msg.channel.send(embed);
-  }
+   const embed = new Discord.MessageEmbed()
+   .setColor('')
 });
 
 client.login('ODE0MDAwMzQ2ODg4MjczOTMw.YDXe5g.6g_jPCQh--PGlAJPJQszd8mKrfw');
