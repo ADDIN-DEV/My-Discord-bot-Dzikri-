00f698a4e514a88ce2fe880ee291d5fee2aafb64
Glitch (hello-express) <none>
2021-03-15 04:52:42 +0000

    🌗😇 Checkpoint ./server.js:28994970/3105

--- server.js
+++ server.js
@@ -1,34 +1,20 @@
-// server.js
-// where your node app starts
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
-
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+client.on('message', msg => {
+  if (msg.content === 'a!p') {
+    msg.reply('Pong!');
+  }
+
+   if (msg.content === 'a!av') {
+    // Send the user's avatar URL
+    msg.reply(msg.author.displayAvatarURL());
+  }
+
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+client.login('ODE0MDAwMzQ2ODg4MjczOTMw.YDXe5g.6g_jPCQh--PGlAJPJQszd8mKrfw');
